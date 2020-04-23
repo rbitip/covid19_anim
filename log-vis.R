a89e8bb4fc16e0b76e5aa8cacb178911239d9743
andywong <medecinqui@gmail.com>
2020-04-23 13:46:22 -0700

    change map-anim framerate; rename log-log to log-vis and add sections for different kinds of visualizations

---
+++ log-vis.R
@@ -0,0 +1,96 @@
+library(here)
+library(lubridate)
+library(dplyr)
+library(reshape2)
+library(magrittr)
+# library(usmap)
+library(ggplot2)
+library(cowplot)
+# library(gganimate)
+
+here <- here::here
+
+filename <- "time_series_covid19_confirmed_US-20200422.csv"
+
+# data loading, fixing/cleaning ----
+
+data <- read.csv(file = here("jh-data", filename), stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
+data <- melt(data, id.vars=c("UID", "iso2", "iso3", "code3", "FIPS", "Admin2", "Province_State", "Country_Region", "Lat", "Long_", "Combined_Key"), variable.name="date", value.name="cases")
+data$date <- sapply(data$date, function(x){substring(x, 2)})
+data$date.day <- mdy(data$date)
+data <- rename(data, date = date.day, date.string = date, state = Province_State)
+
+# diff over N days
+
+N = 4
+
+max_date <- max(data$date)
+min_date <- min(data$date)
+desired_dates <- seq(max_date, min_date, by = -as.difftime(days(N)))
+
+# it's N-day not weekly but it still makes sense
+# weekly has columns: date, cases, growth, relgrowth (growth/cases)
+weekly <- data %>%
+  filter(date %in% desired_dates) %>%
+  group_by(date) %>%
+  summarize(sum(cases)) %>%
+  rename(cases = `sum(cases)`) %>%
+  mutate(growth = cases - lag(cases, default=cases[1])) %>%
+  mutate(relgrowth = growth / cases)
+
+# daily has columns: date, cases
+daily <- data %>%
+  group_by(date) %>%
+  summarize(sum(cases)) %>%
+  rename(cases = `sum(cases)`)
+
+# logistic model ----
+
+nlsmod <- nls(daily$cases ~ SSlogis(as.numeric(daily$date), ph1, ph2, ph3), data = daily)
+summary(nlsmod)
+alpha <- coef(nlsmod)
+f_logis <- function(d) as.numeric(alpha[1]/(1 + exp(-(as.numeric(d) - alpha[2]) / alpha[3])))
+f_logis_10 <- function(d) log10(f_logis(d))
+
+# plotting ----
+
+# plot logistic fit over data - linear
+
+p0 <- ggplot(data = daily, aes(x=date, y=cases)) + 
+  geom_point() + 
+  stat_function(fun=f_logis, color="red") + 
+  theme_light()
+
+print(p0)
+
+# plot logistic fit over data - log
+# starting after March because cases are noisy before March and growth is ~ 0
+# have to use f_logis_10 because stat function is plotted after log scale is applied
+p1 <- ggplot(data = filter(daily, date > ymd("20200301")), aes(x=date, y=cases)) + 
+  geom_point() + 
+  stat_function(fun=f_logis_10) +
+  scale_y_continuous(trans="log10") +
+  theme_light()
+
+print(p1)
+
+# plot of N-day growth vs total cases
+p2 <- ggplot(data=weekly, aes(x=cases, y=growth)) + 
+  geom_point() + 
+  scale_y_continuous(trans="log10") + 
+  scale_x_continuous(trans="log10") + 
+  ylab(paste0(N, "-day growth in cases")) + 
+  theme_light()
+
+print(p2)
+
+# plot of cases and N-day growth vs time
+
+p3 <- ggplot(data = daily, aes(x=date, y=cases)) + 
+  geom_point() + 
+  geom_point(data = weekly, aes(x=date, y=growth), color="red") + 
+  scale_y_continuous(trans="log10", 
+                     sec.axis = sec_axis(~ . * 1, name=paste0(N, "-day growth in cases"))) + 
+  theme_light()
+
+print(p3)
